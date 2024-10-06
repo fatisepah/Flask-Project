55dfe249ffad55b6499ecd612bbf205433321838
Fatemeh Sepahvand <51885589+fatisepah@users.noreply.github.com>
2024-10-07 00:50:47 +0330

    renamed files

---
+++ app1.py
@@ -0,0 +1,23 @@
+#from flask import Flask , render_template
+#
+#
+#app = Flask(__name__)
+#
+#
+#@app.route("/")
+#def index():
+#    return "Hello, this is index page!"
+#
+#@app.route("/hello")
+#def sayHello():
+#    return "Hello fati how are you !"
+#
+##@app.route("/welcome/<name>")
+##def welcome(name):
+##    return f"welcome {name}"
+#
+#
+#@app.route("/welcome/")
+#@app.route("/welcome/<int:id>")
+#def welcome(id=None):
+#    return f"welcome {id}"
